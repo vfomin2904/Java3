86cbfa0706103d69327a9b7799b5ef4a4243d3b2
vfomin2904 <vova-fomin97@mail.ru>
2021-03-18 20:59:12 +0300

    homework 4

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,58 @@
 public class Application {
+    private static Character currentAlpha = 'A';
+    private static Object mon = new Object();
+
     public static void main(String[] args) {
+        new Thread(() -> {
+            for(int i = 0; i< 5; i++) {
+                synchronized (mon) {
+                    while (!currentAlpha.equals('A')) {
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println('A');
+                    currentAlpha = 'B';
+                    mon.notifyAll();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for(int i = 0; i< 5; i++) {
+                synchronized (mon) {
+                    while (!currentAlpha.equals('B')) {
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println('B');
+                    currentAlpha = 'C';
+                    mon.notifyAll();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for(int i = 0; i< 5; i++) {
+                synchronized (mon) {
+                    while (!currentAlpha.equals('C')) {
+                        try {
+                            mon.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println('C');
+                    currentAlpha = 'A';
+                    mon.notifyAll();
+                }
+            }
+        }).start();
 
     }
 }
